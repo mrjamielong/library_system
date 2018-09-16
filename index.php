a2893b2d815fd21d89d6ae60f625eb6df7e3bf2f
Jamie Long <mrjamielong@outlook.com>
2018-09-16 13:27:20 +0100

    start adding core files

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" href="style/main.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+
+        </div>
+        <div class="main_content">
+
+        </div>
+        <div class="footer">
+            
+        </div>
+    </div>
+</body>
+</html>
